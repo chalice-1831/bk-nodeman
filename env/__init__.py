70f94ddc6b747c6f24fd94e5aa692361439b1954
dcd <1151627903@qq.com>
2025-01-08 22:23:58 +0800

    feat:表迁移及调用各系统写入租户ID

--- env/__init__.py
+++ env/__init__.py
@@ -74,6 +74,7 @@ __all__ = [
     "TXY_ENDPOINT",
     # 当前环境为仅GSE2环境
     "BKAPP_IS_PURE_GSE2_VERSION",
+    "ENABLE_MULTI_TENANT_MODE",
 ]
 
 # ===============================================================================
@@ -207,3 +208,5 @@ BKAPP_LEGACY_AUTH = get_type_env(key="BKAPP_LEGACY_AUTH", default=False, _type=b
 BK_NOTICE_ENABLED = get_type_env(key="BK_NOTICE_ENABLED", default=False, _type=bool)
 SYNC_APIGATEWAY_ENABLED = get_type_env(key="SYNC_APIGATEWAY_ENABLED", default=True, _type=bool)
 TXY_ENDPOINT = get_type_env(key="TXY_ENDPOINT", default="", _type=str)
+# 多租户开关
+ENABLE_MULTI_TENANT_MODE = get_type_env(key="ENABLE_MULTI_TENANT_MODE", default=False, _type=bool)
